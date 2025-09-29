4a03bac70bdf17997d426354669f81d60e954a49
MurtazaD1410 <dhariwalamurtaza22@gmail.com>
2025-09-29 18:09:26 +0200

    14: Template Gallery

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar } from "./navbar";
+import { TemplateGallery } from "./template-gallery";
 
 const Home = () => {
   return (
@@ -7,7 +8,9 @@ const Home = () => {
       <div className="fixed top-0 right-0 left-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
-      <div className="mt-16">hello</div>
+      <div className="mt-16">
+        <TemplateGallery />
+      </div>
     </div>
   );
 };
---
+++ src/constants/template.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/blank-document.svg",
+  },
+  {
+    id: "business_letter",
+    label: "Business Letter",
+    imageUrl: "/business-letter.svg",
+  },
+  {
+    id: "cover_letter",
+    label: "Cover Letter",
+    imageUrl: "/cover-letter.svg",
+  },
+  {
+    id: "letter",
+    label: "Letter",
+    imageUrl: "/letter.svg",
+  },
+  {
+    id: "project_proposal",
+    label: "Project Proposal",
+    imageUrl: "/project-proposal.svg",
+  },
+  {
+    id: "software_proposal",
+    label: "Software Proposal",
+    imageUrl: "/software-proposal.svg",
+  },
+  {
+    id: "resume",
+    label: "Resume",
+    imageUrl: "/resume.svg",
+  },
+];
